784be3d85880ac9fd99826334f579a2815c9c164
chen <23054115+cwkang1998@users.noreply.github.com>
2026-05-20 17:16:30 +0800

    fix: breaking system tests from previous changes

--- test/system/shorturls_test.rb
+++ test/system/shorturls_test.rb
@@ -12,31 +12,44 @@ class ShorturlsTest < ApplicationSystemTestCase
   end
 
   test "user creates a shorturl and sees the details page" do
-    with_target_title("CoinGecko Crypto Prices") do
-      visit new_shorturl_path
+    visit root_path
+    target_url = "#{Capybara.current_session.server.base_url}/"
 
-      fill_in "Title", with: "CoinGecko"
-      fill_in "Target URL", with: "https://www.coingecko.com"
-      click_button "Create"
+    visit new_shorturl_path
+    fill_in "Target URL", with: target_url
+    click_button "Create"
 
-      assert_text "CoinGecko Crypto Prices"
-      assert_text "https://www.coingecko.com"
+    assert_text "Site: #{target_url}"
+    assert_text target_url
 
-      shorturl = Shorturl.find_by!(title: "CoinGecko Crypto Prices")
-      assert_current_path shorturl_path(shorturl)
-      assert_text redirect_shorturl_url(shorturl.short_url_code)
-    end
+    shorturl = Shorturl.find_by!(target_url: target_url)
+    assert_current_path shorturl_path(shorturl)
+    assert_text redirect_shorturl_url(shorturl.short_url_code)
+  end
+
+  test "user creates a shorturl with default title when target title cannot be fetched" do
+    visit root_path
+    target_url = "#{Capybara.current_session.server.base_url}/no-title/page"
+
+    visit new_shorturl_path
+    fill_in "Target URL", with: target_url
+    click_button "Create"
+
+    assert_text "Site: #{target_url}"
+    assert_text target_url
+
+    shorturl = Shorturl.find_by!(target_url: target_url)
+    assert_current_path shorturl_path(shorturl)
+    assert_text redirect_shorturl_url(shorturl.short_url_code)
   end
 
   test "user sees validation errors when creation fails" do
     visit new_shorturl_path
 
-    fill_in "Title", with: ""
     fill_in "Target URL", with: "http://localhost/ABC123"
     click_button "Create"
 
     assert_text "Unable to create short URL."
-    assert_text "Title can't be blank"
     assert_text "Target url must not be another shortened URL"
   end
 
@@ -52,19 +65,31 @@ class ShorturlsTest < ApplicationSystemTestCase
     assert_text redirect_shorturl_url(shorturl.short_url_code)
   end
 
+  test "user visits a shorturl and is redirected to the target url" do
+    visit root_path
+    target_url = "#{Capybara.current_session.server.base_url}/"
+    shorturl = Shorturl.create!(title: "Local target", target_url: target_url)
+
+    with_test_geocoder do
+      assert_difference -> { ShorturlAccess.count }, 1 do
+        visit redirect_shorturl_path(shorturl.short_url_code)
+      end
+    end
+
+    assert_current_path root_path
+    assert_link "Create short URL"
+    assert_text "Local target"
+  end
+
   private
 
-  def with_target_title(title)
-    original_call = TargetTitleExtractorService.method(:call)
-
-    TargetTitleExtractorService.define_singleton_method(:call) do |**_kwargs|
-      title
-    end
+  def with_test_geocoder
+    Geocoder.configure(lookup: :test, ip_lookup: :test, cache: nil)
+    Geocoder::Lookup::Test.reset
+    Geocoder::Lookup::Test.add_stub("127.0.0.1", [])
 
     yield
   ensure
-    TargetTitleExtractorService.define_singleton_method(:call) do |*args, **kwargs|
-      original_call.call(*args, **kwargs)
-    end
+    Geocoder::Lookup::Test.reset
   end
 end
